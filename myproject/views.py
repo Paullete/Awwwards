e5da858a10909d7c01a40ebc44c5d2b16c5ca42c
paullete <paulettenereah3@gmail.com>
2022-06-10 17:39:00 +0300

    urls and views paths

--- myproject/views.py
+++ myproject/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render,redirect
 from . models import *
 from .forms import ProjectUpload,UpdateProfileForm
 from django.contrib.auth.decorators import login_required
-from rest_framework import viewsets
-from .serializers import PostSerializer,ProfileSeralizer
 
 def home(request):
     projects = Post.objects.all()
