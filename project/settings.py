c41b18f4e87622fb65c85e7d2a2312bbbfc9624e
paullete <paulettenereah3@gmail.com>
2022-06-10 17:19:56 +0300

    working folders and paths

--- project/settings.py
+++ project/settings.py
@@ -10,10 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
-from pathlib import Path
+from decouple import Config
+# from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myproject.apps.MyprojectConfig',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
